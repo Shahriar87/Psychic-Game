3b5674bdf59793d554cabe4399a35d08a5a1e1b9
Innovator <shahriar.firoz@gmail.com>
2018-08-30 00:39:56 -0500

    Second Commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,44 @@
 var userInput = document.getElementById("user-text");
-var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var computerChoices = "abcdefghijklmnopqrstuvwxyz".split('');
+// console.log(computerChoices);
+
 var guessesLeft = 9;
 var wins = 0;
 var losses = 0;
 var listOfChoices = []
+var computerGuess
+
+function compGuess(){
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    // console.log(computerGuess);
+}
+
+compGuess()
 
 document.onkeyup = function psychic(event) {
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    
     var userChoice = event.key;
     
-    
-    
-
     if (computerChoices.indexOf(userChoice) > -1){
-        if (userChoice === computerGuess){
-            wins ++;
-            guessesLeft = 9; 
-            listOfChoices = [];
-        } if (userChoice !== computerGuess){
+        
+        
+        if (userChoice !== computerGuess){
             guessesLeft --
             listOfChoices.push(userChoice);
 
-
-
-            // userInput.textContent = listOfChoices.concat(push(userChoice));
-
-            // new_array= listOfChoices.concat(listOfChoices);
-
-            // var new_array = listOfChoices.concat();
-
-            //     for (var i = 1; i < listOfChoices.length; i++) {
-            //         new_array[i] = new_array[i-1] + listOfChoices[i];
-            //     }
-
-
         } if (guessesLeft === 0){
-            losses --;
+            losses ++;
             guessesLeft = 9; 
             listOfChoices = [];
-        }
+            compGuess();
+
+        } if (userChoice === computerGuess){
+            wins ++;
+            guessesLeft = 9; 
+            listOfChoices = [];
+            compGuess();
+        } 
 
         document.getElementById("win").textContent = wins;
         document.getElementById("loss").textContent = losses;
@@ -47,7 +47,19 @@ document.onkeyup = function psychic(event) {
 
         
 
+        // -- Some alternative trials --
+
         // userInput.textContent = new_array;
+
+        // userInput.textContent = listOfChoices.concat(push(userChoice));
+
+            // new_array= listOfChoices.concat(listOfChoices);
+
+            // var new_array = listOfChoices.concat();
+
+            //     for (var i = 1; i < listOfChoices.length; i++) {
+            //         new_array[i] = new_array[i-1] + listOfChoices[i];
+            //     }
 
     }
 
